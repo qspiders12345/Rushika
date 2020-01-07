0769b52008c95c5f2cc650dfe49edce7eaf9497e
dell <dell@192.168.1.5>
2020-01-07 16:04:38 +0530

    kalyani plaza !

---
+++ src/main/java/generics/AutoConstant.java
@@ -0,0 +1,14 @@
+package generics;
+
+public interface AutoConstant 
+{
+	String chrome_key="webdriver.chrome.driver";
+	String chrome_value="E:/Softwares/chromedriver.exe";
+	String ie_key="webdriver.ie.driver";
+	String ie_value="E:/Softwares/IEDriverServer.exe";
+	String gecko_key="webdriver.gecko.driver";
+	String gecko_value="E:/Softwares/geckodriver.exe";
+	String url="https://demo.actitime.com/";
+	String sheet_name="ActiTIMEMaven";
+	String excel_path="E:/Ashirvadit/FrameworkData.xlsx";
+}
